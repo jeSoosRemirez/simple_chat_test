c9047e76aa63833098e8f8318c934b49527516d0
jeSoosRemirez <dkhomych0091@gmail.com>
2023-04-14 19:36:11 +0300

    Create admin models for Thread and Message

--- simple_chat/threads/admin.py
+++ simple_chat/threads/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Thread, Message
 
-# Register your models here.
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'header', 'created_time', 'edited_date')
+    list_filter = ('owner', 'created_time')
+    search_fields = ('owner', 'header')
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'thread', 'created_time', 'edited_date')
+    list_filter = ('sender', 'thread', 'created_time')
+    search_fields = ('sender', 'thread')
+
+
+admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Message, MessageAdmin)
